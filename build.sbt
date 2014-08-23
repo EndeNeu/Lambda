27d53e45ed3814ffce9c251b62ccb4d8294d06ff
Emiliano Busiello <emilianobusiello@gmail.com>
2014-08-23 21:21:48 +0200

    Added specs, first tests.

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "lambda"
 version := "1.0"
 
 libraryDependencies += "commons-lang" % "commons-lang" % "2.6"
+
+libraryDependencies += "org.specs2" %% "specs2" % "2.3.10" % "test"
---
+++ src/test/scala/LambdaTest.scala
@@ -0,0 +1,158 @@
+import lambda.LambdaBuilder.{AR, BV, EX}
+import lambda.core.Lambda.{lApply, selfApply, identity}
+import lambda.core.LambdaParser
+import lambda.model.NonEmptyLambdaExpression
+import org.specs2.mutable._
+
+class LambdaTest extends Specification {
+
+  // λx.(λy.y x)
+  val exp2 = EX(BV("x", BV("y", AR("y")),AR("x")))
+
+  // λx.x λy.y
+  val exp4: NonEmptyLambdaExpression = identity("x").:+(identity("y"))
+
+  // x λy.x
+  val exp6 = EX(AR("x"), BV("y", AR("x")))
+
+  // x λx.(λy.y x) λt.t -> x λx.(λy.y λt.t)
+  val exp7 = EX(AR("x"), BV("x", BV("y", AR("y")), AR("x")), BV("t", AR("t")))
+
+  "Lambda constructor" should {
+    "construct" in {
+
+      exp2.getStructure().filter(_ != ' ') must beEqualTo("EX(BV(BV(AR)AR))")
+      exp4.getStructure().filter(_ != ' ') must beEqualTo("EX(BV(AR)BV(AR))")
+      exp6.getStructure().filter(_ != ' ') must beEqualTo("EX(ARBV(AR))")
+      // λx.(x x)
+      selfApply("x").getStructure().filter(_ != ' ') must beEqualTo("EX(BV(ARAR))")
+      // λx.λy.(x y) (z)-> λy.(z y)
+      lApply("x", "y").getStructure().filter(_ != ' ') must beEqualTo("EX(BV(BV(ARAR)))")
+
+      exp7.getStructure().filter(_ != ' ') must beEqualTo("EX(ARBV(BV(AR)AR)BV(AR))")
+    }
+  }
+
+  "toString" should {
+    "stirngify" in {
+
+      true must beEqualTo(true)
+    }
+  }
+
+
+
+
+
+  // λx.x
+  println("λx.x")
+  println(identity("x").toString)
+
+  // λx.(λy.y x)
+  println("λx.(λy.y x)")
+  println(exp2.toString())
+
+  // λx.x λy.y
+  println("λx.x λy.y")
+  println(exp4.toString)
+
+  // λx.(x x)
+  println("λx.(x x)")
+  println(selfApply("x").toString)
+
+  // x λy.x
+  println("x λy.x")
+  println(exp6.toString)
+
+  println("---------------------")
+
+  // λx.x -> z
+  println("λx.x -> z")
+  println(identity("x").betaReduce("z").toString)
+
+  // λx.x λy.y -> z z
+  println("λx.x λy.y -> z z")
+  println(exp4.betaReduce("z").toString)
+
+  //λx.(x x) -> z z
+  println("λx.(x x) -> z z")
+  println(selfApply("x").betaReduce("z").toString)
+
+  // λx.(λy.y x) -> λy.y z
+  println("λx.(λy.y x) -> λy.y z")
+  println(exp2.betaReduce("z").toString)
+
+  // λx.λy.(x y) (z)-> λy.(z y)
+  println("λx.λy.(x y) (z)-> λy.(z y)")
+  println(lApply("x", "y").betaReduce("z").toString)
+
+  // x λy.x -> x x
+  println("x λy.x -> x x")
+  println(exp6.betaReduce("z").toString)
+
+  // λy.(y λy.y) (x) -> x λy.y
+  val exp = EX(BV("y", AR("y"), BV("y", AR("y"))))
+  println("λy.(y λy.y) (x) -> x λy.y")
+  println(exp.betaReduce("x"))
+
+  println("---------------------")
+
+  // λy.(y λy.y) (λx.x) -> λx.x λy.y
+  println("λy.(y λy.y) (λx.x) -> λx.x λy.y")
+  println(exp.betaReduce(EX(BV("x", AR("x")))))
+
+  println("---------------------")
+
+  val test =
+    EX(
+      AR("x"),
+      AR("y"),
+      BV("s",
+        AR("s")
+      ),
+      AR("g")
+    )
+
+  println("flattens:")
+  println("x y λs.s g -> z TODO")
+  println(test.flatten().toString)
+
+  println("---------------------")
+  println("betaReduce()")
+
+  // λx.x λy.y -> λy.y
+  //    println(exp5.betaReduce().toString)
+
+  // λx.x -> λx.x
+  //    println(identity("x").betaReduce().toString)
+
+  // λx.λy.(x y) -> λx.λy.(x y)
+  //    println(lApply("x", "y").betaReduce().toString)
+
+
+  println("x λx.(λy.y x) λt.t -> x λy.y λt.t")
+  println(exp7.betaReduce().toString)
+  println("x λx.(λy.y x) λt.t -> x λt.t")
+  println(exp7.betaReduce(true).toString)
+
+  println("---------------------")
+  println("string parser")
+
+  //    val t: NonEmptyLambdaExpression = LambdaParser.fromString("λx.x")
+  //    println(t.toString)
+
+
+
+  //    println(LambdaParser.fromString("λx.(x y)").toString)
+  // TODO
+  println(LambdaParser.fromString("λx.(λy.(x y))").toString)
+  println(LambdaParser.fromString("λx.(λy.x y)").toString)
+  println(LambdaParser.fromString("λx.y").toString)
+
+  println("---------------------")
+  println("structure")
+
+  println("λx.(λy.(x y))")
+  println(LambdaParser.fromString("λx.(λy.(x y))").getStructure())
+
+}
